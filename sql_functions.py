980c87b1ec6e7e86c8378a4af0302c68c5057e71
Giovanna Sttorto <giovannasttorto@Giovannas-MacBook-Pro.local>
2024-05-09 16:49:25 +0200

    clean flights

--- sql_functions.py
+++ sql_functions.py
@@ -1,8 +1,24 @@
-# We import a method from the  modules to address environment variables and 
-# we use that method in a function that will return the variables we need from .env 
-# to a dictionary we call sql_config
-
 from dotenv import dotenv_values
+import pandas as pd
+import sqlalchemy
+import psycopg2
+
+cities = [
+    "New York, NY",
+    "Boston, MA",
+    "Washington, DC",
+    "Newark, NJ",
+    "Miami, FL",
+    "Houston, TX",
+    "San Francisco, CA",
+    "Seattle, WA"
+]
+cancellation_code = {
+    "A": "mechanical issues",
+    "B": "weather conditions",
+    "C": "traffic control issue",
+    "D": "personal reasons",
+}
 
 def get_sql_config():
     '''
@@ -10,16 +26,10 @@ def get_sql_config():
         returns a dictionary containing the data needed for sqlalchemy.create_engine()
     '''
     needed_keys = ['host', 'port', 'database','user','password']
-    dotenv_dict = dotenv_values(".env")
+    dotenv_dict = dotenv_values("../.env")
     sql_config = {key:dotenv_dict[key] for key in needed_keys if key in dotenv_dict}
     return sql_config
 
-# Import sqlalchemy and pandas - do this only when instructed
-
-import pandas as pd
-import sqlalchemy 
-
-# Insert the get_data() function definition below - do this only when instructed in the notebook
 
 def get_data(query):
    ''' Connect to the PostgreSQL database server, run query and return data'''
@@ -48,7 +58,6 @@ def get_dataframe(sql_query):
     engine = get_engine()
     return pd.read_sql_query(sql_query, engine)
 
-# Insert the get_engine() function definition below - when instructed
 
 def get_engine():
     sql_config = get_sql_config()
@@ -56,23 +65,6 @@ def get_engine():
                         connect_args=sql_config
                         )
     return engine  
-
-cities = [
-    "New York, NY",
-    "Boston, MA",
-    "Washington, DC",
-    "Newark, NJ",
-    "Miami, FL",
-    "Houston, TX",
-    "San Francisco, CA",
-    "Seattle, WA"
-]
-cancellation_code = {
-    "A": "mechanical issues",
-    "B": "weather conditions",
-    "C": "traffic control issue",
-    "D": "personal reasons",
-}
 
 def create_table(year):
     flights_dec = pd.read_csv(f'./data/FLIGHTS_REPORTING_{year}_DEC.csv')
@@ -95,11 +87,9 @@ def create_table(year):
 
     return flights_clean
 
-import psycopg2
-
-def push_to_cloud(table, year):
+def push_to_cloud(table, name):
     schema = 'group3'
-    table_name = f'flights_{year}'
+    table_name = name
 
     engine = get_engine()
 
@@ -119,3 +109,13 @@ def push_to_cloud(table, year):
             engine = None
         else:
             print('No engine')
+
+
+def clean_flights_table(year):
+    table = get_dataframe(f"select * from group3.flights_{year}")
+    table.drop_duplicates(inplace=True)
+    table.rename(columns={'fl_date': 'flight_date'}, inplace=True)
+    table['flight_date'] = pd.to_datetime(table['flight_date'])
+    table["had_delay"] = ((table["arr_delay"] < 0) | (table["dep_delay"] < 0)).astype(int)
+    table.drop(columns=["dep_delay", "arr_delay"], inplace=True)
+    return table
